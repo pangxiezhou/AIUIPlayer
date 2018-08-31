f224a929dd0c296ff13b325199e601445c5f6ece
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-08-31 08:42:46 +0000

    AIUIPlayer initial commit
    git-svn-id: https://svn.iflytek.com:8888/svn/Y_AIUI_CLIENT/Trunk/Development/Source/AIUISolution/sdk/Android/src/AIUI_PB_AS/AIUIPlayer@4516 b3f42486-efa6-2644-a828-f03ee09383db

---
+++ README.md
@@ -0,0 +1,98 @@
+# AIUIPlayer
+
+AIUIPlayer是用于解析播放AIUI语义结果中音频资源的播放器。
+
+## 1. 问题
+
+AIUI语义结果的音频资源在升级后不再直接返回可播放的音频URL，而是需要依用对应信源方的SDK才能解析播放。
+
+举个例子，故事技能在未升级前返回结果如下：
+
+``` json
+{
+	"category": "改变世界100年心灵童话",
+	"name": "学习害怕",
+	"playUrl": "http://od.open.qingting.fm/vod/00/00/0000000000000000000025243697_64.m4a?u=786&channelId=94688&programId=2444135",
+	"series": "学习害怕",
+	"status": "1"
+}
+```
+
+改变后的结果：
+
+``` json
+{
+	"category": "改变世界100年心灵童话",
+	"name": "学习害怕",
+	"resourceId": "94688,2444135",
+	"series": "学习害怕",
+	"source": "qingtingfm",
+	"status": "1"
+}
+```
+
+音频资源不再直接返回可播放的url，而是通过新增的source字段指明信源，resourceId提供特定于信源方的音频信息。
+
+上面这条结果，qingtingfm表明信源方是蜻蜓FM，而resourceId就是"channelId, programId"。蜻蜓FM的SDK中的QTPlayer播放器提供了prepare(channelId, programId)接口播放此类资源。
+
+## 2. 解决办法
+
+AIUIPlayer为开发者提供了统一的播放和控制接口，在内部根据音频资源的source字段，调用不同信源方的SDK进行实际的播放和控制。
+
+## 3. 使用方法
+
+### 3.1 项目配置
+
+在顶层目录下的build.gradle中加入jitPack仓库
+
+``` groovy
+allprojects {
+    repositories {
+        ......
+        maven {
+            url uri('https://jitpack.io')
+        }
+    }
+}
+```
+
+在app下的build.gradle加入AIUIPlayer的依赖
+
+``` groovy
+dependencies {
+    ......
+    implementation 'com.github.pangxiezhou.AIUIPlayer:player_core:0.1'
+}
+```
+
+### 3.2 接口调用
+
+``` kotlin
+    player = AIUIPlayer(this)
+    player.initialize()
+    player.addListener(object : PlayerListener {
+        override fun onPlayerReady() {
+            startPlaySamples()
+        }
+
+        override fun onStateChange(state: PlayState) {
+            when (state) {
+                PlayState.PLAYING -> ToggleBtn.text = "暂停"
+                PlayState.PAUSED -> ToggleBtn.text = "继续"
+            }
+        }
+
+        override fun onMediaChange(item: MetaInfo) {
+            //根据播放项变化回调修改title内容
+            titleTxt.text = item.title
+        }
+    })
+    
+    PreBtn.setOnClickListener {
+            if (player.previous()) {
+                Toast.makeText(this, "当前已是第一首", Toast.LENGTH_LONG).show()
+            }
+        }
+```
+
+更详细的调用参考app目录下代码示例。
---
+++ player_core/src/main/kotlin/com/iflytek/aiui/player/core/players/MetaQTPlayer.kt
@@ -0,0 +1,130 @@
+package com.iflytek.aiui.player.core.players
+
+import fm.qingting.qtsdk.QTSDK
+import fm.qingting.qtsdk.play.ICallback
+import fm.qingting.qtsdk.play.QTPlay
+import fm.qingting.qtsdk.player.QTPlayer
+import android.content.Context
+import com.iflytek.aiui.player.core.MetaInfo
+import com.iflytek.aiui.player.init.PlayerInitializer
+
+//蜻蜓FM初始化接口，方便测试需要
+typealias QTCallback = (QTPlayer) -> Unit
+
+typealias QTPlayerInitializer = (readyCallback: QTCallback) -> Unit
+
+
+/**
+ * Meta播放器 蜻蜓FM实现
+ *
+ * 内部通过调用蜻蜓FM SDK的接口实现播放和暂停继续的控制
+ */
+class MetaQTPlayer(context: Context) : MetaAbstractPlayer() {
+    private var player: QTPlayer? = null
+    private var mPlayState = QTPlayer.PlayState.NONE
+    private var initializer: QTPlayerInitializer = {
+        QTSDK.setHost("https://open.staging.qingting.fm")
+        PlayerInitializer.initQTFM(context)
+        QTSDK.setAuthRedirectUrl("http://qttest.qingting.fm")
+        QTSDK.Debug = true
+        QTPlay.initial(ICallback { _, _ ->
+            it(QTSDK.getPlayer())
+        })
+    }
+
+    override fun initialize() {
+        super.initialize()
+        initializer?.invoke {
+            player = it
+            stateChange(MetaState.READY)
+            player?.addListener(object : QTPlayer.StateChangeListener {
+                override fun onPlayDurationChange(duration: Int) {
+                }
+
+                override fun onPlayProgressChange(p0: Int) {
+
+                }
+
+                override fun onPlayStateChange(state: Int) {
+                    when (state) {
+                        QTPlayer.PlayState.PLAYING -> {
+                            stateChange(MetaState.PLAYING)
+                        }
+
+                        QTPlayer.PlayState.PAUSED -> {
+                            stateChange(MetaState.PAUSED)
+                        }
+
+                        //播放完成
+                        QTPlayer.PlayState.EOF -> {
+                            stateChange(MetaState.COMPLETE)
+                        }
+                    }
+
+                    mPlayState = state
+                }
+            })
+        }
+
+    }
+
+
+    override fun play(info: MetaInfo):Boolean {
+        if(!canDispose(info)) return false
+
+        val data = info.info
+        //通过resourceId字段拆分出channelID和ProgramID
+        //完整广播节目只包含ChannelID
+        val resourceId = data.optString("resourceId")
+        val temp = resourceId.split(",")
+        val channelID = temp[0].trim().toInt()
+        val programID = if (temp.size > 1) {
+            temp[1].trim().toInt()
+        } else {
+            -1
+        }
+
+        mPlayState = QTPlayer.PlayState.NONE
+        if (programID == -1) {
+            player?.prepare(channelID)
+        } else {
+            player?.prepare(channelID, programID)
+        }
+
+        return true
+    }
+
+    override fun pause() {
+        player?.pause()
+    }
+
+    override fun resume() {
+        player?.play()
+    }
+
+    override fun release() {
+        player?.stop()
+        stateChange(MetaState.IDLE)
+        super.release()
+    }
+
+    private fun canDispose(item: MetaInfo): Boolean {
+        val source = item.info.optString("source")
+        if (source == "qingtingfm") {
+            val resourceId = item.info.optString("resourceId", "")
+            if (!resourceId.isEmpty()) {
+                val resourceIDs = resourceId.split(",")
+                return resourceIDs.all {
+                    try {
+                        it.trim().toInt()
+                        true
+                    } catch (e: NumberFormatException) {
+                        false
+                    }
+                }
+            }
+        }
+
+        return false
+    }
+}
