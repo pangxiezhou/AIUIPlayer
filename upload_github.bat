109dc0576c19b6cca84c114a8d40c4ba4bcbc7f4
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-01 09:29:46 +0000

    github上传脚本完善
    git-svn-id: https://svn.iflytek.com:8888/svn/Y_AIUI_CLIENT/Trunk/Development/Source/AIUISolution/sdk/Android/src/AIUI_PB_AS/AIUIPlayer@4519 b3f42486-efa6-2644-a828-f03ee09383db

--- upload_github.bat
+++ upload_github.bat
@@ -2,47 +2,46 @@
 
 chcp 65001
 
-git branch | find "* master" > NUL & IF ERRORLEVEL 0 (
-    ECHO 处于master分支
-	
+git branch | find "* master" > NUL & IF ERRORLEVEL 1 (
+	ECHO 警告：不处于master分支，此命令仅支持在master分支运行
+) ELSE (
 	setlocal
 	:PROMPT
 	SET /P AREYOUSURE=确认同步代码到github吗 (Y/[N])
 	IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 	
-	echo 同步代码到svn
+	echo 1. 同步代码到svn
 	git svn dcommit
 	
-	if not ERRORLEVEL 0 (
-		echo 同步代码到SVN出错，请自行检查
+	if ERRORLEVEL 1 (
+		echo 同步代码到SVN出错，检查后重试
 		GOTO END
+	) ELSE (
+		echo 同步代码到SVN成功
 	)
 	
-	echo 切换到clean分支
-	git checkout clean
-	git rebase master
-	
-	echo 删除重建github分支
-	git branch -D github
+	echo 2. 删除重建github分支
+	git branch | find "* github" > NUL & IF ERRORLEVEL 0 (
+		git branch -D github
+	)
 	git checkout -b github
 	
-	echo 删除敏感文件
-	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch doc" --prune-empty --tag-name-filter cat -- --all
-	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch upload_github.bat" --prune-empty --tag-name-filter cat -- --all
-	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch player_init/src/main/cpp" --prune-empty --tag-name-filter cat -- --all
-	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch player_init/CMakeLists.txt" --prune-empty --tag-name-filter cat -- --all
+	echo 3. 删除敏感文件
+	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch doc" --prune-empty --tag-name-filter cat
+	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch player_init/src/main/cpp" --prune-empty --tag-name-filter cat 
+	git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch player_init/CMakeLists.txt" --prune-empty --tag-name-filter cat 
 
-	echo 添加github remote
+	echo 4. 添加github remote
 	git remote | find "github_origin" > NUL & IF ERRORLEVEL 0 (
 		git remote remove github_origin
 	)
 	git remote add github_origin git@github.com:pangxiezhou/AIUIPlayer.git
 	
-	echo 同步到github
+	echo 5. 同步到github
 	git push -f -u --tags github_origin github:master
 	
-) ELSE (
-    ECHO 不处于master分支，此命令仅支持在master分支运行
+	echo 6. 切换回master分支
+	git checkout master
 )
 
 :END
