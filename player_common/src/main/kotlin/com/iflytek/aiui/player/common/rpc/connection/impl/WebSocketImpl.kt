7e13232703e4c3f5aa102672f4276c764e3aaef1
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-19 11:02:24 +0000

    增加WebSocketConnection的测试

--- player_common/src/main/kotlin/com/iflytek/aiui/player/common/rpc/connection/impl/WebSocketImpl.kt
+++ player_common/src/main/kotlin/com/iflytek/aiui/player/common/rpc/connection/impl/WebSocketImpl.kt
@@ -8,47 +8,93 @@ import org.java_websocket.handshake.ServerHandshake
 import org.java_websocket.server.WebSocketServer
 import java.net.InetSocketAddress
 import java.net.URI
+import java.util.*
 
-class WebSocketServerConnection(val port: Int, val connectionListener: ServerConnectionListener) : DataConnection() {
-    private var activeConnection: WebSocket? = null
-    private val server: WebSocketServer = object : WebSocketServer(InetSocketAddress(port)) {
-        override fun onOpen(conn: WebSocket?, handshake: ClientHandshake?) {
-            activeConnection = conn
-        }
+abstract class ServerConnectionListener {
+    open fun onStart() {
 
-        override fun onClose(conn: WebSocket?, code: Int, reason: String?, remote: Boolean) {
-            if (activeConnection == conn) activeConnection = null
-        }
+    }
+}
 
-        override fun onMessage(conn: WebSocket?, message: String?) {
-            onData(message)
-        }
+abstract class ClientConnectionListener {
+    open fun onOpen() {
 
-        override fun onStart() {
-            connectionListener.onStart()
-        }
+    }
 
-        override fun onError(conn: WebSocket?, ex: Exception?) {
+    open fun onClose() {
+
+    }
+}
+
+class WebSocketServerConnection(private val port: Int, val connectionListener: ServerConnectionListener) : DataConnection() {
+    private lateinit var server: WebSocketServer
+
+    private fun initServer() {
+        server = object : WebSocketServer(InetSocketAddress(port)) {
+            override fun onOpen(conn: WebSocket?, handshake: ClientHandshake?) {
+                println("server on client connected")
+            }
+
+            override fun onClose(conn: WebSocket?, code: Int, reason: String?, remote: Boolean) {
+                println("server on client disconnected")
+            }
+
+            override fun onMessage(conn: WebSocket?, message: String?) {
+                onData(message)
+            }
+
+            override fun onStart() {
+                println("server on Start")
+                connectionListener.onStart()
+            }
+
+            override fun onError(conn: WebSocket?, ex: Exception?) {
+                println("server on error $ex")
+            }
         }
     }
 
-    fun start() {
+    override fun start() {
+        initServer()
         server.start()
     }
 
+    override fun stop() {
+        server.stop()
+    }
+
     override fun send(data: String) {
-        activeConnection?.send(data)
+        server.broadcast(data)
     }
 
 }
 
-class WebSocketClientConnection(host: String, port: Int, val connectionListener: ClientConnectionListener) : DataConnection() {
-    private val client = object : WebSocketClient(URI("ws://$host:$port")) {
+class WebSocketClientConnection(private val host: String, private val port: Int, val connectionListener: ClientConnectionListener) : DataConnection() {
+    private val sendQueue = mutableListOf<String>()
+    private lateinit var client: WebSocketClient
+
+    private fun initClient() {
+        client = object : WebSocketClient(URI("ws://$host:$port")) {
         override fun onOpen(handshakedata: ServerHandshake?) {
+            println("on Open")
             connectionListener.onOpen()
+            if (!sendQueue.isEmpty()) {
+                sendQueue.removeAll {
+                    send(it)
+                    true
+                }
+            }
         }
 
         override fun onClose(code: Int, reason: String?, remote: Boolean) {
+            println("On Close")
+            connectionListener.onClose()
+            Timer().schedule(object: TimerTask() {
+                override fun run() {
+                    reconnect()
+                }
+            }, 100)
+            println("start reconnect")
         }
 
         override fun onMessage(message: String?) {
@@ -56,22 +102,26 @@ class WebSocketClientConnection(host: String, port: Int, val connectionListener:
         }
 
         override fun onError(ex: Exception?) {
+            println("onError $ex")
         }
     }
+    }
 
-    fun connect() {
+    override fun start() {
+        initClient()
         client.connect()
     }
 
     override fun send(data: String) {
-        client.send(data)
+        if (!client.isOpen) {
+            sendQueue.add(data)
+        } else {
+            client.send(data)
+        }
+    }
+
+    override fun stop() {
+        client.close()
     }
 }
 
-interface ServerConnectionListener {
-    fun onStart()
-}
-
-interface ClientConnectionListener {
-    fun onOpen()
-}
