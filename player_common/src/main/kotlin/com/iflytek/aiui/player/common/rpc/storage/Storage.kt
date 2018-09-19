64dc7cf9c0e75821f10711a7fb63339b7a80a203
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-19 11:02:48 +0000

    增加Storage

---
+++ player_common/src/main/kotlin/com/iflytek/aiui/player/common/rpc/storage/Storage.kt
@@ -0,0 +1,25 @@
+package com.iflytek.aiui.player.common.rpc.storage
+
+import android.content.Context
+
+class Storage(context: Context) {
+    private val sharedPreferences = context.getSharedPreferences("player_storage", Context.MODE_PRIVATE)
+
+    fun put(key: String, value: String) {
+        sharedPreferences.edit().putString(key, value).commit()
+    }
+
+    fun put(key: String, value: Int) {
+        sharedPreferences.edit().putInt(key, value).commit()
+    }
+
+    fun getString(key: String): String? {
+        return sharedPreferences.getString(key, "")
+    }
+
+    fun getInteger(key: String): Int {
+        return sharedPreferences.getInt(key, 0)
+    }
+
+
+}
