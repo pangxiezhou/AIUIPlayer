739f03af161fd9dc3390bae96d4290c78e2ff685
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-19 11:02:56 +0000

    PlayerInitializer增加酷狗SDK初始化
    git-svn-id: https://svn.iflytek.com:8888/svn/Y_AIUI_CLIENT/Trunk/Development/Source/AIUISolution/sdk/Android/src/AIUI_PB_AS/AIUIPlayer@4694 b3f42486-efa6-2644-a828-f03ee09383db

--- player_init/src/main/cpp/player_init.cpp
+++ player_init/src/main/cpp/player_init.cpp
@@ -7,15 +7,16 @@
 
 #include "player_init.h"
 
-char mix[] = "ZYTTQF2hNNTDkYwyNNGTUUttNMmjMY1wOYSi00xzMZWmUQ31LLTTkgywMZ2mYIttMYDTAEx1NYjTNkl1MMDDAIy0MMGjFhkj";
+char qtKey[] = "ZYTTQF2hNNTDkYwyNNGTUUttNMmjMY1wOYSi00xzMZWmUQ31LLTTkgywMZ2mYIttMYDTAEx1NYjTNkl1MMDDAIy0MMGjFhkj";
+char kuGouKey[] = "29UqRDzSCIZqOkgXIEjXsnsZ3EmPuXXmYP06";
 
 JNIEXPORT void JNICALL Java_com_iflytek_aiui_player_init_PlayerInitializer_initQTFM(JNIEnv *env, jclass thisClass, jobject context){
     char _client_id[49], _key[49];
 
     int cursor = 0;
-    for(int index = 0; index < strlen(mix); index+=2) {
-        _client_id[cursor] = mix[index];
-        _key[cursor] = mix[index + 1];
+    for(int index = 0; index < strlen(qtKey); index+=2) {
+        _client_id[cursor] = qtKey[index];
+        _key[cursor] = qtKey[index + 1];
         cursor++;
     }
 
@@ -29,5 +30,35 @@ JNIEXPORT void JNICALL Java_com_iflytek_aiui_player_init_PlayerInitializer_initQ
     jmethodID initMethod = env->GetStaticMethodID(QTSDKClass, "init", "(Landroid/content/Context;Ljava/lang/String;)V");
 
     env->CallStaticVoidMethod(QTSDKClass, initMethod, context, client_ID);
+}
+
+
+JNIEXPORT void JNICALL Java_com_iflytek_aiui_player_init_PlayerInitializer_initKuGouMusic(JNIEnv *env, jclass thisClass){
+    char _app_id[5], _key[33];
+
+    _app_id[0] = kuGouKey[0];
+    _app_id[1] = kuGouKey[1];
+    _app_id[2] = kuGouKey[strlen(kuGouKey) - 2];
+    _app_id[3] = kuGouKey[strlen(kuGouKey) - 1];
+    _app_id[4] = '\0';
+
+    int cursor = 0;
+    for(int index = 2; index < strlen(kuGouKey) - 2; index+=1) {
+        _key[cursor++] = kuGouKey[index];
+    }
+    _key[32] = '\0';
+
+
+    jstring appId = env->NewStringUTF(_app_id);
+    jstring appKey = env->NewStringUTF(_key);
+
+    jclass kgSDKClass=env->FindClass("com/kugou/kgmusicsdk/KGMusicSDK");
+    jmethodID kgGetInstance = env->GetStaticMethodID(kgSDKClass, "sharedInstance", "()Lcom/kugou/kgmusicsdk/KGMusicSDK;");
+    jobject kgSDKInstance = env->CallStaticObjectMethod(kgSDKClass, kgGetInstance);
+
+    jclass kgMusicSDK = env->GetObjectClass(kgSDKInstance);
+    jmethodID registerWithAppid = env->GetMethodID(kgMusicSDK, "registerWithAppID", "(Ljava/lang/String;Ljava/lang/String;)V");
+
+    (env)->CallVoidMethod(kgSDKInstance, registerWithAppid, appId, appKey);
 }
 
