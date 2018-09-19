dd2a03997b590d65be250a4e296010606a9bac8c
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-19 11:02:30 +0000

    WebSocketConnection重构

--- player_common/src/main/kotlin/com/iflytek/aiui/player/common/rpc/RPC.kt
+++ player_common/src/main/kotlin/com/iflytek/aiui/player/common/rpc/RPC.kt
@@ -1,5 +1,6 @@
 package com.iflytek.aiui.player.common.rpc
 
+import com.iflytek.aiui.player.common.rpc.connection.ConnectionListener
 import com.iflytek.aiui.player.common.rpc.connection.DataConnection
 import com.iflytek.aiui.player.common.rpc.method.Request
 import org.json.JSONObject
@@ -12,10 +13,18 @@ interface RPCListener {
 
 class RPC(private val dataConnection: DataConnection, private val rpcListener: RPCListener) {
     private val callbackMap = hashMapOf<Int, RPCCallback<Any>>()
+    private val sendQueue = mutableListOf<String>()
 
     init {
-        dataConnection.registerDataCallback  { message ->
-            if (message != null)  {
+        dataConnection.registerConnectionListener(object: ConnectionListener() {
+            override fun onActive() {
+                sendQueue.removeAll {
+                    dataConnection.send(it)
+                    true
+                }
+            }
+
+            override fun onData(message: String) {
                 val data = JSONObject(message)
                 if (data.has("result")) {
                     callbackMap[data.optInt("id")]?.invoke(data.optString("result"))
@@ -23,12 +32,17 @@ class RPC(private val dataConnection: DataConnection, private val rpcListener: R
                     rpcListener.onRequest(this@RPC, message)
                 }
             }
-        }
+
+            override fun onDeactivate() {
+            }
+        })
     }
 
     fun <T> request(req: Request, callback: RPCCallback<T>) {
         callbackMap[req.id] = callback as RPCCallback<Any>
-        dataConnection.send(req.toJSONString())
+        if(!dataConnection.active || dataConnection.send(req.toJSONString())) {
+            sendQueue.add(req.toJSONString())
+        }
     }
 
     fun <T> response(req: Request, value: T) {
@@ -37,7 +51,8 @@ class RPC(private val dataConnection: DataConnection, private val rpcListener: R
         description.put("id", req.id)
         description.put("result", value.toString())
 
-        dataConnection.send(description.toString())
+        if(!dataConnection.active || dataConnection.send(description.toString())) {
+            sendQueue.add(description.toString())
+        }
     }
-
 }
--- player_common/src/test/kotlin/com/iflytek/aiui/player/common/rpc/connection/WebSocketConnectionTest.kt
+++ player_common/src/test/kotlin/com/iflytek/aiui/player/common/rpc/connection/WebSocketConnectionTest.kt
@@ -1,21 +1,40 @@
 package com.iflytek.aiui.player.common.rpc.connection
 
-import com.iflytek.aiui.player.common.rpc.connection.impl.ClientConnectionListener
-import com.iflytek.aiui.player.common.rpc.connection.impl.ServerConnectionListener
+import com.iflytek.aiui.player.common.rpc.connection.impl.ClientInitializer
 import com.iflytek.aiui.player.common.rpc.connection.impl.WebSocketClientConnection
 import com.iflytek.aiui.player.common.rpc.connection.impl.WebSocketServerConnection
+import com.iflytek.aiui.player.common.rpc.connection.impl.ServerInitializer
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.server.WebSocketServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Spy
+import org.mockito.junit.MockitoJUnitRunner
 import java.util.concurrent.CountDownLatch
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
+@RunWith(MockitoJUnitRunner::class)
 class WebSocketConnectionTest {
     private val port = 4096
-    private lateinit var server: DataConnection
-    private lateinit var client: DataConnection
 
-    private fun setUp() {
+    private val server = WebSocketServerConnection(port)
+    private val client = WebSocketClientConnection("localhost", port)
+
+
+    @Before
+    fun setUp() {
         val countStartDown = CountDownLatch(1)
-        server = WebSocketServerConnection(port, object : ServerConnectionListener() {
+        server.registerConnectionListener(object: ConnectionListener() {
             override fun onStart() {
                 countStartDown.countDown()
             }
@@ -24,8 +43,8 @@ class WebSocketConnectionTest {
         countStartDown.await()
 
         val countConnectionDown = CountDownLatch(1)
-        client = WebSocketClientConnection("localhost", port, object : ClientConnectionListener() {
-            override fun onOpen() {
+        client.registerConnectionListener(object: ConnectionListener() {
+            override fun onActive() {
                 countConnectionDown.countDown()
             }
         })
@@ -33,29 +52,89 @@ class WebSocketConnectionTest {
         countConnectionDown.await()
     }
 
-    private fun tearDown() {
+
+    @After
+    fun tearDown() {
         client.stop()
         server.stop()
+
+    }
+    @Test(timeout = 2000)
+    fun start() {
+        assertTrue(server.active)
+        assertTrue(client.active)
     }
 
     @Test(timeout = 2000)
-    fun recv() {
-        setUp()
+    fun clientClose() {
+        client.stop()
 
+        val countClientDeActivateDown = CountDownLatch(1)
+        client.registerConnectionListener(object : ConnectionListener() {
+            override fun onDeactivate() {
+                countClientDeActivateDown.countDown()
+            }
+        })
+
+        val countServerDeactivateDown = CountDownLatch(1)
+        server.registerConnectionListener(object: ConnectionListener() {
+            override fun onDeactivate() {
+                countServerDeactivateDown.countDown()
+            }
+        })
+
+        countClientDeActivateDown.await()
+        countServerDeactivateDown.await()
+
+        assertFalse(server.active)
+        assertFalse(client.active)
+    }
+
+    @Test(timeout = 2000)
+    fun serverClose() {
+        server.stop()
+
+        val countClientDeActivateDown = CountDownLatch(1)
+        client.registerConnectionListener(object : ConnectionListener() {
+            override fun onDeactivate() {
+                countClientDeActivateDown.countDown()
+            }
+        })
+
+        val countServerDeactivateDown = CountDownLatch(1)
+        server.registerConnectionListener(object: ConnectionListener() {
+            override fun onDeactivate() {
+                countServerDeactivateDown.countDown()
+            }
+        })
+
+        countClientDeActivateDown.await()
+        countServerDeactivateDown.await()
+
+        assertFalse(server.active)
+        assertFalse(client.active)
+    }
+
+    @Test(timeout = 2000)
+    fun communicate() {
         val clientContent = "client_foobar"
         val serverContent = "server_foobar"
 
         val countServerRecvDown = CountDownLatch(1)
-        server.registerDataCallback {
-            assertEquals(it, clientContent)
-            countServerRecvDown.countDown()
-        }
+        server.registerConnectionListener(object: ConnectionListener() {
+            override fun onData(data: String) {
+                assertEquals(data, clientContent)
+                countServerRecvDown.countDown()
+            }
+        })
 
         val countClientRecvDown = CountDownLatch(1)
-        client.registerDataCallback {
-            assertEquals(it, serverContent)
-            countClientRecvDown.countDown()
-        }
+        client.registerConnectionListener(object: ConnectionListener() {
+            override fun onData(data: String) {
+                assertEquals(data, serverContent)
+                countClientRecvDown.countDown()
+            }
+        })
 
         client.send(clientContent)
         server.send(serverContent)
@@ -66,47 +145,42 @@ class WebSocketConnectionTest {
         tearDown()
     }
 
-    @Test
+    @Test(timeout = 2000)
     fun reconnect() {
-        var countStartDown = CountDownLatch(1)
-        server = WebSocketServerConnection(port, object : ServerConnectionListener() {
-            override fun onStart() {
-                countStartDown.countDown()
-            }
-        })
-        server.start()
-        countStartDown.await()
-
-        val countOpenDown = CountDownLatch(1)
-        val countCloseDown = CountDownLatch(1)
-        client = WebSocketClientConnection("localhost", port, object : ClientConnectionListener() {
-            override fun onOpen() {
-                countOpenDown.countDown()
-            }
-
-            override fun onClose() {
-                countCloseDown.countDown()
-            }
-        })
-        client.start()
-        countOpenDown.await()
-
         server.stop()
-        countCloseDown.await()
+        val countClientDeactivateDown = CountDownLatch(1)
+        client.registerConnectionListener(object : ConnectionListener() {
+            override fun onDeactivate() {
+                countClientDeactivateDown.countDown()
+            }
+        })
+        countClientDeactivateDown.await()
+        assertFalse(server.active)
+        assertFalse(client.active)
 
-        val clientContent = "client_foobar"
-        val countServerRecvDown = CountDownLatch(1)
-        client.send(clientContent)
         server.start()
-        server.registerDataCallback {
-            assertEquals(it, clientContent)
-            countServerRecvDown.countDown()
-        }
+        val countClientActiveDown = CountDownLatch(1)
+        client.registerConnectionListener(object : ConnectionListener() {
+            override fun onActive() {
+                countClientActiveDown.countDown()
+            }
+        })
+        countClientActiveDown.await()
+        assertTrue(server.active)
+        assertTrue(client.active)
+    }
 
-        countServerRecvDown.await()
-
-
+    @Ignore
+    @Test(timeout = 2000)
+    fun noReconnectWhenStop() {
         client.stop()
-        server.stop()
+
+        val countClientDeactivateDown = CountDownLatch(1)
+        client.registerConnectionListener(object : ConnectionListener() {
+            override fun onDeactivate() {
+                countClientDeactivateDown.countDown()
+            }
+        })
+        countClientDeactivateDown.await()
     }
 }
