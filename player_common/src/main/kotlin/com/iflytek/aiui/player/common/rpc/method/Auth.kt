40cb6fccce16cc9966d7fb284b4974a4c211fba4
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-19 11:03:04 +0000

    GetToken实现Parcelable接口
    git-svn-id: https://svn.iflytek.com:8888/svn/Y_AIUI_CLIENT/Trunk/Development/Source/AIUISolution/sdk/Android/src/AIUI_PB_AS/AIUIPlayer@4696 b3f42486-efa6-2644-a828-f03ee09383db

--- player_common/src/main/kotlin/com/iflytek/aiui/player/common/rpc/method/Auth.kt
+++ player_common/src/main/kotlin/com/iflytek/aiui/player/common/rpc/method/Auth.kt
@@ -1,20 +1,11 @@
 package com.iflytek.aiui.player.common.rpc.method
 
+import android.os.Parcel
+import android.os.Parcelable
 import org.json.JSONObject
 
 
-class GetToken(from: String?, serializeData: String? = null): Request(serializeData){
-    companion object {
-        fun forSource(source: String): GetToken {
-           return GetToken(source)
-        }
-
-        fun deserializeFrom(serializeData: String): GetToken {
-            return GetToken(null, serializeData)
-        }
-    }
-
-
+class GetToken(from: String?, serializeData: String? = null): Request(serializeData), Parcelable{
     val source: String?
 
     init {
@@ -24,5 +15,41 @@ class GetToken(from: String?, serializeData: String? = null): Request(serializeD
 
     override fun methodName(): String {
         return "getAuth"
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(source)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<GetToken> {
+        override fun createFromParcel(parcel: Parcel): GetToken {
+            val description = JSONObject()
+
+            description.put("jsonrpc", "2.0")
+            description.put("id", parcel.readInt())
+            description.put("method", "getAuth")
+            description.put("params", JSONObject(hashMapOf(
+                    "source" to parcel.readString()
+            )))
+
+            return deserializeFrom(description.toString())
+        }
+
+        override fun newArray(size: Int): Array<GetToken?> {
+            return arrayOfNulls(size)
+        }
+
+        fun forSource(source: String): GetToken {
+            return GetToken(source)
+        }
+
+        fun deserializeFrom(serializeData: String): GetToken {
+            return GetToken(null, serializeData)
+        }
     }
 }
