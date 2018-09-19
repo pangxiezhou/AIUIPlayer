9fd480230d4c776d153fb2abbafed4ee52a42e61
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-19 11:02:18 +0000

    包名重构
    git-svn-id: https://svn.iflytek.com:8888/svn/Y_AIUI_CLIENT/Trunk/Development/Source/AIUISolution/sdk/Android/src/AIUI_PB_AS/AIUIPlayer@4682 b3f42486-efa6-2644-a828-f03ee09383db

--- player_common/src/test/kotlin/com/iflytek/aiui/player/common/rpc/RPCTest.kt
+++ player_common/src/test/kotlin/com/iflytek/aiui/player/common/rpc/RPCTest.kt
@@ -1,39 +1,64 @@
 package com.iflytek.aiui.player.common.rpc
 
+import com.iflytek.aiui.player.common.rpc.connection.impl.ClientConnectionListener
+import com.iflytek.aiui.player.common.rpc.connection.impl.ServerConnectionListener
+import com.iflytek.aiui.player.common.rpc.connection.impl.WebSocketClientConnection
+import com.iflytek.aiui.player.common.rpc.connection.impl.WebSocketServerConnection
+import com.iflytek.aiui.player.common.rpc.method.GetToken
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import java.util.concurrent.CountDownLatch
 
 class RPCTest {
-    @Test()
-    fun invoke() {
-        val port = 4096
-        val countTokenRecvDown = CountDownLatch(1)
-        var server: RPCServer? = null
-        server = RPCServer(port, object : RPCListener {
-            override fun onRequest(rpc: RPC, data: String) {
-            }
-        }, object : ServerConnectionListener {
+    private val port = 4096
+    private lateinit var client: WebSocketClientConnection
+    private lateinit var server: WebSocketServerConnection
+
+    @Before
+    fun setUp() {
+        //Server和Client Connection建立
+        val countStartDown = CountDownLatch(1)
+        server = WebSocketServerConnection(port, object : ServerConnectionListener {
             override fun onStart() {
-                val fakeToken = "foobar"
-                val client = RPCClient(port, object : RPCListener {
-                    override fun onRequest(rpc: RPC, data: String) {
-                        val req = GetToken.deserializeFrom(data)
-                        rpc.response(req, fakeToken)
-                    }
-                }, object : ClientConnectionListener {
-                    override fun onOpen() {
-                        val req = GetToken.forSource("qingting")
-                        server?.request<String>(req) {
-                            assertEquals(it, fakeToken)
-                            countTokenRecvDown.countDown()
-                        }
-                    }
-                })
-                client.connect()
+                countStartDown.countDown()
             }
         })
         server.start()
+        countStartDown.await()
+
+        val countConnectionDown = CountDownLatch(1)
+        client = WebSocketClientConnection("localhost", port, object : ClientConnectionListener {
+            override fun onOpen() {
+                countConnectionDown.countDown()
+            }
+        })
+        client.connect()
+        countConnectionDown.await()
+    }
+
+
+    @Test()
+    fun invoke() {
+        val countTokenRecvDown = CountDownLatch(1)
+        val fakeToken = "foobar"
+        val serverRPC = RPC(server, object: RPCListener {
+            override fun onRequest(rpc: RPC, data: String) {
+            }
+        })
+
+        val clientRPC = RPC(client, object: RPCListener {
+            override fun onRequest(rpc: RPC, data: String) {
+                val req = GetToken.deserializeFrom(data)
+                rpc.response(req, fakeToken)
+            }
+
+        })
+
+        serverRPC.request<String>(GetToken.forSource("qingting")) { token ->
+            countTokenRecvDown.countDown()
+            assertEquals(token, fakeToken)
+        }
 
         countTokenRecvDown.await()
     }
