cffa892a0d34f9d79ad7eb3672228e049c507e74
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-09-19 11:03:01 +0000

    增加酷我登录页面
    git-svn-id: https://svn.iflytek.com:8888/svn/Y_AIUI_CLIENT/Trunk/Development/Source/AIUISolution/sdk/Android/src/AIUI_PB_AS/AIUIPlayer@4695 b3f42486-efa6-2644-a828-f03ee09383db

---
+++ player_auth/src/main/kotlin/com/iflytek/aiui/player/auth/PlayerRemoteService.kt
@@ -0,0 +1,39 @@
+package com.iflytek.aiui.player.auth
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.iflytek.aiui.player.common.rpc.RPC
+import com.iflytek.aiui.player.common.rpc.RPCListener
+import com.iflytek.aiui.player.common.rpc.connection.impl.WebSocketClientConnection
+import com.iflytek.aiui.player.common.rpc.method.GetToken
+import org.json.JSONObject
+
+class PlayerRemoteService:Service() {
+    private lateinit var clientRPC: RPC
+
+    override fun onBind(p0: Intent?): IBinder? {
+       return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val connection = WebSocketClientConnection("localhost", 4096)
+        clientRPC = RPC(connection, object: RPCListener {
+            override fun onRequest(rpc: RPC, data: String) {
+                val req = JSONObject(data)
+                when(req.optString("method")) {
+                    "getAuth" -> {
+                        val getAuthReq = GetToken.deserializeFrom(data)
+                        when(getAuthReq.source) {
+                            "kugou" -> {
+
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
