e0ad9289244ee2ee233f31df3a9c08c4a4532886
ruipeng <ruipeng@b3f42486-efa6-2644-a828-f03ee09383db>
2018-11-30 03:20:58 +0000

    子播放器可配置
    git-svn-id: https://svn.iflytek.com:8888/svn/Y_AIUI_CLIENT/Trunk/Development/Source/AIUISolution/sdk/Android/src/AIUI_PB_AS/AIUIPlayer@5178 b3f42486-efa6-2644-a828-f03ee09383db

---
+++ sub_players/kugou/src/test/kotlin/com/iflytek/aiui/player/players/MetaKGPlayerTest.kt
@@ -0,0 +1,308 @@
+package com.iflytek.aiui.player.players
+
+import android.content.Context
+import android.media.MediaPlayer
+import com.iflytek.aiui.player.common.error.ErrorDef
+import com.iflytek.aiui.player.common.player.MediaPlayerInitializer
+import com.iflytek.aiui.player.common.player.MetaItem
+import com.iflytek.aiui.player.common.player.MetaListener
+import com.iflytek.aiui.player.common.player.MetaState
+import com.iflytek.aiui.player.common.rpc.*
+import com.iflytek.aiui.player.common.storage.Storage
+import com.kugou.common.utils.KgInfo
+import com.nhaarman.mockitokotlin2.*
+import org.json.JSONObject
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Spy
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MetaKGPlayerTest {
+    private val mediaPlayer: MediaPlayer = mock()
+
+    @Spy
+    private val mInitializer: MediaPlayerInitializer = { callback ->
+        callback(mediaPlayer)
+    }
+    @Mock
+    private lateinit var mKuGouAPI: KuGouAPI
+
+
+    private val context: Context = mock()
+    private val mRPC: RPC = mock()
+    private val storageMap = hashMapOf<String, Any>()
+    private val storage: Storage = mock()
+
+    @InjectMocks
+    private val player = MetaKGPlayer(context, mRPC, storage)
+
+    private val fakeURL = "fake.url"
+    private val fakeUserID = 112358132
+    private val fakeToken = "fake.token"
+
+
+    private fun setUpKuGouAPIMock() {
+        var hasLogin = false
+        whenever(mKuGouAPI.login(any(), any())).then {
+            hasLogin  = true
+            true
+        }
+        whenever(mKuGouAPI.retrieveUrl(any(), any())).then {
+            val kgInfo = KgInfo()
+            if(hasLogin) {
+                kgInfo.errorNo = 0
+                kgInfo.Info = fakeURL
+            } else {
+                kgInfo.errorNo = -2
+                kgInfo.Info = "token expire"
+            }
+            kgInfo
+        }
+    }
+
+    private fun setUpStorageMock() {
+        whenever(storage.put(any(), any<String>())).then {
+            storageMap.put(it.arguments[0] as String, it.arguments[1])
+        }
+
+        whenever(storage.put(any(), any<Int>())).then {
+            storageMap.put(it.arguments[0] as String, it.arguments[1])
+        }
+
+        whenever(storage.getString(any())).then {
+            storageMap[it.arguments[0]] ?: ""
+        }
+
+        whenever(storage.getInteger(any())).then {
+            storageMap[it.arguments[0]] ?: 0
+        }
+    }
+
+    private fun setUp() {
+        setUpKuGouAPIMock()
+        setUpStorageMock()
+
+        whenever(mRPC.request<String>(any(), any(), any(), any())).then {
+            (it.arguments[1] as SuccessCallback<String>)("$fakeUserID#$fakeToken")
+        }
+
+
+        player.initialize()
+    }
+
+
+    @Test
+    fun initialize() {
+        setUp()
+
+        verify(mKuGouAPI).init()
+    }
+
+    @Test
+    fun canDispose() {
+        setUp()
+
+        assertTrue(player.canDispose(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to "1234567889"
+        )), "story")))
+
+        assertFalse(player.canDispose(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "qingting",
+                "itemid" to "1234567889"
+        )), "story")))
+
+        assertFalse(player.canDispose(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "qingtingfm",
+                "playUrl" to "http://fake.url/test.mp3"
+        )), "story")))
+
+        assertFalse(player.canDispose(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to ""
+        )), "story")))
+    }
+
+    @Test
+    fun play() {
+        setUp()
+
+        val itemID = "1234567889"
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to itemID
+        )), "story"))
+
+        verify(mRPC).request<String>(any(), any(), any(), any())
+        verify(mKuGouAPI).login(fakeUserID, fakeToken)
+        verify(mKuGouAPI, times(2)).retrieveUrl(itemID, 0)
+    }
+
+    @Test
+    fun rpcError() {
+        setUp()
+
+        val listener = mock<MetaListener>()
+        player.addListener(listener)
+
+        var errorCallback: ErrorCallback? = null
+        whenever(mRPC.request<String>(any(), any(), any(), any())).then { it ->
+            //只存储，不立即回调
+            errorCallback = it.arguments[2] as ErrorCallback
+            true
+        }
+
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to "112358132134"
+        )), "story"))
+
+        errorCallback?.invoke(ErrorDef.ERROR_RPC_RESET, "rpc peer reset")
+
+        verify(listener).onStateChange(MetaState.ERROR)
+        verify(listener).onError(ErrorDef.ERROR_RPC_RESET, "rpc peer reset")
+        verify(mKuGouAPI, never()).login(fakeUserID, fakeToken)
+        verify(mRPC).request<String>(any(), any(), any(), any())
+    }
+
+
+    @Test
+    fun tokenCache() {
+        setUp()
+
+        var hasLogin = false
+        whenever(mKuGouAPI.login(any(), any())).then {
+            hasLogin  = true
+            true
+        }
+        whenever(mKuGouAPI.retrieveUrl(any(), any())).then {
+            val kgInfo = KgInfo()
+            if(hasLogin) {
+                kgInfo.errorNo = 0
+                kgInfo.Info = fakeURL
+            } else {
+                kgInfo.errorNo = -2
+                kgInfo.Info = "token expire"
+            }
+            kgInfo
+        }
+
+        val itemId = "112358132134"
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to itemId
+        )), "story"))
+
+        verify(mRPC).request<String>(any(), any(), any(), any())
+        verify(mKuGouAPI).login(fakeUserID, fakeToken)
+
+        clearInvocations(mRPC, mKuGouAPI)
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to itemId
+        )), "story"))
+
+        verify(mKuGouAPI).retrieveUrl(itemId, 0)
+        verify(mKuGouAPI, never()).login(fakeUserID, fakeToken)
+        verify(mRPC, never()).request<String>(any(), any(), any(), any())
+
+        clearInvocations(mRPC, mKuGouAPI)
+        hasLogin = false
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to itemId
+        )), "story"))
+
+        verify(mKuGouAPI, times(2)).retrieveUrl(itemId, 0)
+        verify(mKuGouAPI).login(fakeUserID, fakeToken)
+        //使用本地存储的token缓存
+        verify(mRPC, never()).request<String>(any(), any(), any(), any())
+
+        clearInvocations(mRPC, mKuGouAPI)
+        hasLogin = false
+        storageMap.clear()
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to itemId
+        )), "story"))
+
+        verify(mKuGouAPI, times(2)).retrieveUrl(itemId, 0)
+        verify(mKuGouAPI).login(fakeUserID, fakeToken)
+        //本地存储token清空，重新rpc请求
+        verify(mRPC).request<String>(any(), any(), any(), any())
+
+    }
+
+    @Test
+    fun onlyOneActiveRequest() {
+        setUp()
+
+        var errorCallback: ErrorCallback? = null
+        whenever(mRPC.request<String>(any(), any(), any(), any())).then { it ->
+            //只存储，不立即回调
+            errorCallback = it.arguments[2] as ErrorCallback
+            true
+        }
+
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to "112358132134"
+        )), "story"))
+
+        verify(mKuGouAPI, never()).login(fakeUserID, fakeToken)
+        verify(mRPC).request<String>(any(), any(), any(), any())
+
+
+        clearInvocations(mRPC, mKuGouAPI)
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to "112358132134"
+        )), "story"))
+
+        verify(mRPC, never()).request<String>(any(), any(), any(), any())
+        verify(mKuGouAPI, never()).login(fakeUserID, fakeToken)
+
+        clearInvocations(mRPC, mKuGouAPI)
+        errorCallback?.invoke(ErrorDef.ERROR_RPC_RESET, "rpc peer reset")
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to "112358132134"
+        )), "story"))
+
+        verify(mRPC).request<String>(any(), any(), any(), any())
+    }
+
+    @Test
+    fun tokenOnCurrent() {
+        setUp()
+
+        var callback: SuccessCallback<String>? = null
+        whenever(mRPC.request<String>(any(), any(), any(), any())).then { it ->
+            //只存储，不立即回调
+            callback = it.arguments[1] as SuccessCallback<String>
+            true
+        }
+
+        val firstItem = "112358132134"
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to firstItem
+        )), "story"))
+
+        val secondItem = "fake second hash"
+        player.play(MetaItem(JSONObject(hashMapOf<String,Any>(
+                "source" to "kugou",
+                "itemid" to secondItem
+        )), "story"))
+
+        clearInvocations(mKuGouAPI)
+        callback?.invoke("$fakeUserID#$fakeToken")
+
+        verify(mKuGouAPI).login(fakeUserID, fakeToken)
+        verify(mKuGouAPI).retrieveUrl(secondItem, 0)
+    }
+}
---
+++ sub_players/qingting/src/main/kotlin/com/iflytek/aiui/player/players/MetaQTPlayer.kt
@@ -0,0 +1,164 @@
+package com.iflytek.aiui.player.players
+
+import fm.qingting.qtsdk.QTSDK
+import fm.qingting.qtsdk.play.QTPlay
+import fm.qingting.qtsdk.player.QTPlayer
+import android.content.Context
+import com.iflytek.aiui.player.common.rpc.RPC
+import com.iflytek.aiui.player.common.error.ErrorDef
+import com.iflytek.aiui.player.common.player.MetaAbstractPlayer
+import com.iflytek.aiui.player.common.player.MetaItem
+import com.iflytek.aiui.player.common.player.MetaState
+import com.iflytek.aiui.player.common.storage.Storage
+import fm.qingting.qtsdk.callbacks.QTCallback
+
+//蜻蜓FM初始化接口，方便测试需要
+typealias QTInitializeCallback = (QTPlayer) -> Unit
+
+typealias QTPlayerInitializer = (readyCallback: QTInitializeCallback) -> Unit
+
+
+/**
+ * Meta播放器 蜻蜓FM实现
+ *
+ * 内部通过调用蜻蜓FM SDK的接口实现播放和暂停继续的控制
+ */
+class MetaQTPlayer(context: Context, rpc: RPC, storage: Storage) : MetaAbstractPlayer(context, rpc, storage) {
+    private var player: QTPlayer? = null
+    private var mQTSDKInitialized = false
+    private var mPaused = true
+    private var initializer: QTPlayerInitializer = {
+        //因为蜻蜓SDK目前不提供销毁接口，重复初始化报错，故只只初始化一次
+        if(!mQTSDKInitialized) {
+            QTSDK.Debug  = true
+            QTSDK.setHost("https://api.open.qingting.fm")
+            QTPlayerNative.initQTFM(context)
+            QTPlay.initial(QTCallback { success, _ ->
+                if(success) {
+                    it(QTSDK.getPlayer())
+                    mQTSDKInitialized = true
+                }
+            })
+        } else {
+            it(QTSDK.getPlayer())
+        }
+    }
+
+    override fun initialize() {
+        super.initialize()
+        initializer.invoke {
+            player = it
+            player?.addListener(object : QTPlayer.StateChangeListener {
+                override fun onPlayDurationChange(duration: Int) {
+                }
+
+                override fun onPlayProgressChange(p0: Int) {
+
+                }
+
+                override fun onPlayStateChange(state: Int) {
+                    when (state) {
+                        QTPlayer.PlayState.PLAYING -> {
+                            if(state() == MetaState.LOADING || state() == MetaState.PLAYING) {
+                                stateChange(MetaState.PLAYING)
+                            } else {
+                                player?.pause()
+                            }
+                        }
+
+                        //播放完成
+                        QTPlayer.PlayState.EOF -> {
+                            stateChange(MetaState.COMPLETE)
+                        }
+
+                        //播放源错误
+                        QTPlayer.PlayState.SOURCEFAIL -> {
+                            onError(ErrorDef.ERROR_QT_SOURCE_FAILED, "QTPlayer Loading failed")
+                        }
+
+                        else -> {
+
+                        }
+                    }
+                }
+            })
+            stateChange(MetaState.READY)
+        }
+
+    }
+
+
+    override fun play(item: MetaItem) {
+        val data = item.info
+        //通过resourceId字段拆分出channelID和ProgramID
+        //完整广播节目只包含ChannelID
+        val resourceId = data.optString("resourceId")
+        val temp = resourceId.split(",")
+        val channelID = temp[0].trim().toInt()
+        val programID = if (temp.size > 1) {
+            temp[1].trim().toInt()
+        } else {
+            -1
+        }
+
+        mPaused = false
+        stateChange(MetaState.LOADING)
+        if (programID == -1) {
+            player?.prepare(channelID)
+        } else {
+            player?.prepare(channelID, programID)
+        }
+    }
+
+    override fun pause() {
+        if(state() == MetaState.PLAYING) {
+            player?.pause()
+        }
+        stateChange(MetaState.PAUSED)
+
+    }
+
+    override fun resume() {
+        if(state() == MetaState.PAUSED) {
+            player?.play()
+        }
+        stateChange(MetaState.PLAYING)
+    }
+
+    override fun release() {
+        player?.stop()
+        stateChange(MetaState.IDLE)
+        super.release()
+    }
+
+    override fun canDispose(item: MetaItem): Boolean {
+        if (item.source == "qingtingfm") {
+            val resourceId = item.info.optString("resourceId", "")
+            if (!resourceId.isEmpty()) {
+                val resourceIDs = resourceId.split(",")
+                return resourceIDs.all {
+                    try {
+                        it.trim().toInt()
+                        true
+                    } catch (e: NumberFormatException) {
+                        false
+                    }
+                }
+            }
+        }
+
+        return false
+    }
+
+    override fun getDuration(): Int {
+        return player?.duration ?: 0
+    }
+
+    override fun getCurrentPos(): Int {
+        return player?.position?: 0
+    }
+
+    override fun seekTo(msec: Int) {
+        player?.seekTo(msec)
+    }
+}
